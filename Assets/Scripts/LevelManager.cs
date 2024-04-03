82e04f65ae224f786fccad40b801445f6281ed97
dattateja99 <anakala@usc.edu>
2024-04-03 10:50:28 -0700

    No Analytics
    Use This till Next Tuesday

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,35 +1,35 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour
-{
-    public static LevelManager Instance { set; get;}
+// public class LevelManager : MonoBehaviour
+// {
+//     public static LevelManager Instance { set; get;}
 
-    public int CurrentLevelNumber { get; private set; }
+//     public int CurrentLevelNumber { get; private set; }
 
-    private void Start()
-    {
-        Instance = this;
+//     private void Start()
+//     {
+//         Instance = this;
 
-    }
+//     }
 
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
+//     private void OnEnable()
+//     {
+//         SceneManager.sceneLoaded += OnSceneLoaded;
+//     }
 
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
+//     private void OnDisable()
+//     {
+//         SceneManager.sceneLoaded -= OnSceneLoaded;
+//     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.name.StartsWith("Level"))
-        {
-            // Assuming your level scenes are named with a format "LevelX"
-            // Where X is the level number
-            CurrentLevelNumber = int.Parse(scene.name.Substring(6)); // Extracts the number from the scene name
-        }
-    }
-}
+//     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+//     {
+//         if (scene.name.StartsWith("Level"))
+//         {
+//             // Assuming your level scenes are named with a format "LevelX"
+//             // Where X is the level number
+//             CurrentLevelNumber = int.Parse(scene.name.Substring(6)); // Extracts the number from the scene name
+//         }
+//     }
+// }
