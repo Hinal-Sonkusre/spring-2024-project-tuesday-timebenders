bc80759a6e388832148fd198a90559af6e476106
dattateja99 <anakala@usc.edu>
2024-04-09 01:35:08 -0700

    Commented Player ID
    Player Id commented

--- Assets/Scripts/PlayerID.cs
+++ Assets/Scripts/PlayerID.cs
@@ -1,23 +1,23 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class PlayerID : MonoBehaviour
-{
-    public string ID { get; private set; }
+// public class PlayerID : MonoBehaviour
+// {
+//     public string ID { get; private set; }
 
-    void Awake()
-    {
-        // Retrieve the existing player ID or generate a new one if it doesn't exist
-        ID = PlayerPrefs.GetString("PlayerID", string.Empty);
+//     void Awake()
+//     {
+//         // Retrieve the existing player ID or generate a new one if it doesn't exist
+//         ID = PlayerPrefs.GetString("PlayerID", string.Empty);
 
-        if (string.IsNullOrEmpty(ID))
-        {
-            // No ID was found, generate a new one and save it
-            ID = System.Guid.NewGuid().ToString();
-            PlayerPrefs.SetString("PlayerID", ID);
-            PlayerPrefs.Save(); // Make sure to save the PlayerPrefs changes
-        }
+//         if (string.IsNullOrEmpty(ID))
+//         {
+//             // No ID was found, generate a new one and save it
+//             ID = System.Guid.NewGuid().ToString();
+//             PlayerPrefs.SetString("PlayerID", ID);
+//             PlayerPrefs.Save(); // Make sure to save the PlayerPrefs changes
+//         }
 
-        // For demonstration purposes, let's log the player ID
-        Debug.Log("Player ID: " + ID);
-    }
-}
+//         // For demonstration purposes, let's log the player ID
+//         Debug.Log("Player ID: " + ID);
+//     }
+// }
